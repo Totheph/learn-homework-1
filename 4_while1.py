684442e9cec78b37494f5b27e09fac67baa59d6a
Totheph <low.mikow@gmail.com>
2023-09-06 19:46:48 +0300

    Forth exercise

--- 4_while1.py
+++ 4_while1.py
@@ -3,19 +3,16 @@
 Домашнее задание №1
 
 Цикл while: hello_user
-
-* Напишите функцию hello_user(), которая с помощью функции input() спрашивает 
-  пользователя “Как дела?”, пока он не ответит “Хорошо”
-   
 """
 
-
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-
+  while True:
+    answer = input("Как дела?")
+    if answer == "Хорошо":
+      break
+      
+hello_user()
+  
     
 if __name__ == "__main__":
     hello_user()
